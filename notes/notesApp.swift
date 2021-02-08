c529c13e86177b98561323727d4d5bdeafb677a7
Shivam Dhuria <sdhuria@ask-fast.com>
2021-02-08 13:48:48 +0530

    Initial Commit

---
+++ notes/notesApp.swift
@@ -0,0 +1,17 @@
+//
+//  notesApp.swift
+//  notes
+//
+//  Created by Shivam Dhuria on 08/02/21.
+//
+
+import SwiftUI
+
+@main
+struct notesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
